f91f6cc5902feb31e57ff9f48685a99d28b9d2ed
limseungkyu <corla@fastlane.kr>
2022-09-20 22:47:36 +0900

    BindingAdapter 추가

---
+++ app/src/main/java/com/sklim/binding/Adapter.kt
@@ -0,0 +1,31 @@
+package com.sklim.binding
+
+import androidx.appcompat.widget.AppCompatImageView
+import androidx.databinding.BindingAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
+import com.sklim.adapter.ImageAdapter
+
+object Adapter {
+
+    @BindingAdapter("adapter")
+    @JvmStatic
+    fun RecyclerView.setAdapter(adapter: ImageAdapter?) {
+        this.adapter = adapter
+    }
+
+    @BindingAdapter("url")
+    @JvmStatic
+    fun AppCompatImageView.setUrl(path: String?) {
+        path?.also {
+            Glide.with(context).load(path)
+                .diskCacheStrategy(DiskCacheStrategy.NONE)
+                .skipMemoryCache(true)
+                .override(width, height)
+                .centerCrop()
+                .transition(DrawableTransitionOptions.withCrossFade()).into(this)
+        }
+    }
+}
