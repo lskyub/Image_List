f5bcf842e7d38a0a57694b7bce7dd84e1c107ab8
limseungkyu <corla@fastlane.kr>
2022-09-20 22:48:44 +0900

    viewmodel에서 usecase를 통해 api 호출

--- app/src/main/java/com/sklim/ui/ListViewModel.kt
+++ app/src/main/java/com/sklim/ui/ListViewModel.kt
@@ -1,12 +1,29 @@
 package com.sklim.ui
 
+import android.util.Log
 import androidx.lifecycle.DefaultLifecycleObserver
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.sklim.adapter.ImageAdapter
+import com.sklim.domain.model.Images
+import com.sklim.domain.usecase.ImagesUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class ListViewModel @Inject constructor(
+    private val useCase: ImagesUseCase
 ) : ViewModel(), DefaultLifecycleObserver {
 
+    val adapter = ImageAdapter()
+
+    init {
+        viewModelScope.launch {
+            useCase.fetchImageList(Images.RQ(1)).collectLatest {
+                adapter.submitData(it)
+            }
+        }
+    }
 }
